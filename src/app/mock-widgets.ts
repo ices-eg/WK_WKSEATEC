d7a1559ee026dba038a1ee4a165bf974cff685c7
CIAN MC SWEENEY <ciano1000@gmail.com>
2019-06-21 09:07:30 +0100

    Changed mock widgets a bit

--- src/app/mock-widgets.ts
+++ src/app/mock-widgets.ts
@@ -7,5 +7,5 @@ export const WIDGETS: Widget[] =[
     {id:4,name:"Widget 4",imageURL:"https://github.com/davidcurrie2001/DatrasQCLengthWeightDistribution/blob/master/Screenshot.png?raw=true",widgetURL:"http://10.11.1.70:3845/",author:"David Currie"},
     {id:5,name:"Widget 5",imageURL:"https://github.com/davidcurrie2001/DatrasQCLengthWeightDistribution/blob/master/Screenshot.png?raw=true",widgetURL:"http://10.11.1.70:3845/",author:"David Currie"},
     {id:6,name:"Outliers",imageURL:"https://github.com/davidcurrie2001/DatrasQCLengthWeightDistribution/blob/master/Screenshot.png?raw=true",widgetURL:"http://10.11.1.70:3845/",author:"David Currie"},
-    {id:7,name:"Widget 7",imageURL:"https://github.com/davidcurrie2001/DatrasQCLengthWeightDistribution/blob/master/Screenshot.png?raw=true",widgetURL:"http://10.11.1.70:3845/",author:"David Currie"}
+    {id:7,name:"Widget 7",imageURL:"https://github.com/davidcurrie2001/DatrasQCLengthWeightDistribution/blob/master/Screenshot.png?raw=true",widgetURL:" http://10.11.1.70:3846/",author:"David Currie"}
 ];
