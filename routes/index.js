7a78d0b1dd9cddaed9573238c98600ef7ccd8708
ciano1000 <feasbur3@Marine.ie>
2019-07-05 11:15:47 +0100

    Changed localhost to compose Value

--- routes/index.js
+++ routes/index.js
@@ -69,7 +69,7 @@ router.get("/get-widget-url/", function (req, res, next) {
   var containerName = req.query.name;
   var splitNames = containerName.split(/[:/]/);
   var name = splitNames[1];
-  var rootURL = 'http://localhost:'
+  var rootURL = 'express:3000/'
 
   var options = {
     all: true, limit: 1, filters: { name: [name] }
