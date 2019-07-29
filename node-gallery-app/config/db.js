515a125a8ab8a128ac6d6a77d3838c3a6b10e20b
CIAN MC SWEENEY <ciano1000@gmail.com>
2019-07-29 15:11:09 +0100

    Fixed offline SQL error

--- node-gallery-app/config/db.js
+++ node-gallery-app/config/db.js
@@ -1,18 +1,22 @@
 const key = require('./keys').SQLURI;
 const sql = require('mssql');
+const isOffline = require('../data_access/config_reader').config.isOffline;
 console.log("From db:"+key);
 /**
  * Create our connection to the sql server
  * Pool promises prevent errors when making concurrent queries
  * Each query waits until the previous one has completed
  */
-const poolPromise = new sql.ConnectionPool(key)
+var poolPromise = null;
+if(!isOffline){
+    poolPromise = new sql.ConnectionPool(key)
     .connect()
     .then(pool => {
         console.log("SQL Connected")
         return (pool);
     })
     .catch(err => console.log("No SQL connection", err))
+}
 
 module.exports = {
     sql, poolPromise
--- node-gallery-app/config/keys.js
+++ node-gallery-app/config/keys.js
@@ -1,15 +1,19 @@
 var fs = require('fs');
 const path = require('path');
+const isOffline = require('../data_access/config_reader').config.isOffline;
 const directory = path.normalize(process.cwd()+'/keys/keys.json');
-var keys;
-try{
-let rawdata = fs.readFileSync(directory);
-keys = JSON.parse(rawdata);
-console.log(keys.SQLURI);
+var keys = {};
+if(!isOffline){
+    try{
+        let rawdata = fs.readFileSync(directory);
+        keys = JSON.parse(rawdata);
+        console.log(keys.SQLURI);
+        }
+        catch(err){
+            console.log("dir:" + directory +"err:"+err);
+        }
 }
-catch(err){
-    console.log("dir:" + directory +"err:"+err);
-}
+
 module.exports ={
     SQLURI: keys.SQLURI
 }
